ff801b8002e54facce2766b6f216dd2f1bab9fda
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2022-05-03 15:49:36 -0500

    :sparkles: Add picture in picture

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('Hello World');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function init() {
+  selectMediaStream();
+
+  button.addEventListener('click', async () => {
+    button.disable = true;
+    await videoElement.requestPictureInPicture();
+    button.disable = false;
+  });
+}
+
+init();
